1f01ed1dc9ed6bb165bbf8e6b3b8a83335392406
Radek Warowny <65827302+radekwarowny@users.noreply.github.com>
2020-06-17 11:45:47 +0100

    Delete exercise_18.py

--- exercise_18.py
+++
@@ -1,6 +0,0 @@
-# Age in Five Years
-
-name = input("Hello. What is your name?\n")
-age = int(input("Hi, {}! How old are you?\n".format(name)))
-print("Did you know that in five years you will be {} years old?".format(age+5))
-print("And five years ago you were {}! Imagine that!".format(age-5))
