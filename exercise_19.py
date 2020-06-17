144561b561c5a179b2648a9b0ca6db0f45f17d3c
Radek Warowny <65827302+radekwarowny@users.noreply.github.com>
2020-06-17 11:45:59 +0100

    Delete exercise_19.py

--- exercise_19.py
+++
@@ -1,8 +0,0 @@
-# A Dumb Calculator
-
-num_1 = float(input("What is your first number: "))
-num_2 = float(input("What is your second number: "))
-num_3 = float(input("What is your third number: "))
-total = (num_3 + num_2 + num_3) / 2
-print()
-print("({:.2f} + {:.2f} + {:.2f}) / 2 is... {:.2f} ".format(num_1, num_2, num_3, total))
