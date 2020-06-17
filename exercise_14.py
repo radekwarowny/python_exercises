c12036ca61bb7edfb2329c41c199dc10694906eb
Radek Warowny <65827302+radekwarowny@users.noreply.github.com>
2020-06-17 11:45:22 +0100

    Delete exercise_14.py

--- exercise_14.py
+++
@@ -1,8 +0,0 @@
-# Asking Questions
-
-age = (input("How old are you?\n"))
-height = (input("How tall are you? (inches)\n"))
-weight = (input("How much do you weigh? (pounds)\n"))
-
-
-print("So you're {} old, {} inches tall and {} lbs heavy.".format(age, height, weight))
