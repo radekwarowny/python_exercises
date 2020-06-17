1ed3fec6b6b67a29b310247c2d80fedd5fd719cb
Radek Warowny <65827302+radekwarowny@users.noreply.github.com>
2020-06-17 11:46:12 +0100

    Delete exercise_20.py

--- exercise_20.py
+++
@@ -1,8 +0,0 @@
-# BMI Calculator
-
-height = float(input("Your height in m: "))
-weight = int(input("Your weight in kg: "))
-print()
-bmi = weight / (height * height)
-print()
-print("Your BMI is {:.1f}.".format(bmi))
