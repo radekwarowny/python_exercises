e0f65ef2226a190f2fd85b8d36e057d85ae25aaf
Radek Warowny <65827302+radekwarowny@users.noreply.github.com>
2021-04-05 12:51:33 +0100

    Update codewars.py

--- codewars/codewars.py
+++ codewars/codewars.py
@@ -87,7 +87,7 @@ print(person.age)
 
 
 """ challenge 4
-n this kata, your job is to create a class Dictionary which you can add words to and their entries. 
+In this kata, your job is to create a class Dictionary which you can add words to and their entries. 
 Example:
 
 >>> d = Dictionary()
@@ -113,6 +113,101 @@ d.newentry('Apple', 'A fruit that grows on trees')
 
 print(d.look('Apple'))
 print(d.look('Banana'))
+
+""" challenge 5
+A company is opening a bank, but the coder who is designing the user class made some errors. They need you to help them.
+
+You must include the following:
+
+    A withdraw method
+        Subtracts money from balance
+        One parameter, money to withdraw
+        Raise a ValueError if there isn't enough money to withdraw
+        Return a string with name and balance(see examples)
+
+    A check method
+        Adds money to balance
+        Two parameters, other user and money
+        Other user will always be valid
+        Raise a ValueError if other user doesn't have enough money
+        Raise a ValueError if checking_account isn't true for other user
+        Return a string with name and balance plus other name and other balance(see examples)
+
+    An add_cash method
+        Adds money to balance
+        One parameter, money to add
+        Return a string with name and balance(see examples)
+
+Additional Notes:
+
+    Checking_account should be stored as a boolean
+    No input numbers will be negative
+    Output must end with a period
+    Float numbers will not be used so, balance should be integer
+    No currency will be used
+
+Examples:
+
+Jeff = User('Jeff', 70, True)
+Joe = User('Joe', 70, False)
+
+Jeff.withdraw(2) # Returns 'Jeff has 68.'
+
+Joe.check(Jeff, 50) # Returns 'Joe has 120 and Jeff has 18.'
+
+Jeff.check(Joe, 80) # Raises a ValueError
+
+Joe.checking_account = True # Enables checking for Joe
+
+Jeff.check(Joe, 80) # Returns 'Jeff has 98 and Joe has 40'
+
+Joe.check(Jeff, 100) # Raises a ValueError
+
+Jeff.add_cash(20.00) # Returns 'Jeff has 118.'
+"""
+
+class User:
+    def __init__(self, name, balance, checking_account):
+        self.name = name
+        self.balance = balance
+        self.checking_account = checking_account
+        
+    def withdraw(self, value):
+        if self.balance < value: raise ValueError()
+        self.balance -= value
+        return "{} has {}.".format(self.name, self.balance)
+    
+    def add_cash(self, value):
+        self.balance += value
+        return "{} has {}.".format(self.name, int(self.balance))
+    
+    def check(self, other, value):
+        if other.checking_account:
+            s1, s2 = other.withdraw(value), self.add_cash(value)
+            return "{} and {}".format(s2, s1)
+        raise ValueError()
+    
+
+    
+Jeff = User('Jeff', 70, True)   
+Joe = User('Joe', 70, False)
+
+Jeff = User('Jeff', 70, True)
+Joe = User('Joe', 70, False)
+
+Jeff.withdraw(2) # Returns 'Jeff has 68.'
+
+Joe.check(Jeff, 50) # Returns 'Joe has 120 and Jeff has 18.'
+
+Jeff.check(Joe, 80) # Raises a ValueError
+
+Joe.checking_account = True # Enables checking for Joe
+
+Jeff.check(Joe, 80) # Returns 'Jeff has 98 and Joe has 40'
+
+Joe.check(Jeff, 100) # Raises a ValueError
+
+Jeff.add_cash(20.00) # Returns 'Jeff has 118.'
 
 
 
