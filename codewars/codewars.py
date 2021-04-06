c700e3b78fbf8aa749ffa4f674921c351c59136f
Radek Warowny <65827302+radekwarowny@users.noreply.github.com>
2021-04-06 12:09:43 +0100

    Update codewars.py

--- codewars/codewars.py
+++ codewars/codewars.py
@@ -209,7 +209,66 @@ Joe.check(Jeff, 100) # Raises a ValueError
 
 Jeff.add_cash(20.00) # Returns 'Jeff has 118.'
 
+""" challenge 6
+A keyword cipher is a monoalphabetic cipher which uses a "keyword" to provide encryption. 
+It is somewhat similar to a Caesar cipher.
+In a keyword cipher, repeats of letters in the keyword are removed and the alphabet is reordered such that the letters in the keyword appear first, 
+followed by the rest of the letters in the alphabet in their otherwise usual order.
 
+E.g. for an uppercase latin alphabet with keyword of "KEYWORD":
+
+A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z
+
+becomes:
+
+K|E|Y|W|O|R|D|A|B|C|F|G|H|I|J|L|M|N|P|Q|S|T|U|V|X|Z
+
+such that:
+
+cipher.encode('ABCHIJ') == 'KEYABC'
+cipher.decode('KEYABC') == 'ABCHIJ'
+
+All letters in the keyword will also be in the alphabet. 
+For the purpose of this kata, only the first occurence of a letter in a keyword should be used. 
+Any characters not provided in the alphabet should be left in situ when encoding or decoding.
+"""
+
+class keyword_cipher(object):
+    def __init__(self, abc, keyword):
+        self.abc = abc
+        self.keyword = keyword
+        self.code = []
+        
+        for i in keyword:
+            self.code.append(i)
+        
+        for i in abc: 
+            if i not in keyword: self.code.append(i)
+                
+    def encode(self, plain):
+        result = []
+        for i in plain:
+            index = self.abc.index(i)
+            print(self.code[index], end='')
+            result.append(self.code[index])
+        return ''.join(result)
+    
+    def decode(self, ciphered):
+        result = []
+        for i in ciphered:
+            index = self.code.index(i)
+            print(self.abc[index], end='')
+            result.append(self.abc[index])
+        return ''.join(result)
+
+abc = "abcdefghijklmnopqrstuvwxyz"
+key = "keyword"
+cipher = keyword_cipher(abc, key)
+
+cipher.encode("abc") # "key");
+cipher.encode("xyz") # "vxz");
+cipher.decode("key") # "abc");
+cipher.decode("vxz") # "xyz");
 
 
 
